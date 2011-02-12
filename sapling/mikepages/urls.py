4c584d4b83bef39c65541356f017b2faeec77243
Mike Ivanov <mivanov@gmail.com>
2011-02-11 16:04:02 -0800

    Fixes #35
    Now using class-based generic views (ListView, DetailView and UpdateView).  To get the behavior we want when an object is not found, created the Custom404Mixin and CreateObjectMixin.

--- sapling/mikepages/urls.py
+++ sapling/mikepages/urls.py
@@ -1,20 +1,17 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
+from django.views.generic import ListView
 
 import views
 from models import Page
 
-page_info = {
-    'queryset': Page.objects.all(),
-    'template_name': 'mikepages/page_list.html',
-    'template_object_name': 'page',
+page_list_info = {
+    'model': Page,
+    'context_object_name': 'page_list',
 }
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^diffs/', include('diffs.foo.urls')),
-    url(r'^$', list_detail.object_list, page_info, 'title-index'),
-    url(r'^(?P<slug>[-\w]+)$', views.show, name='show-page'),
+    url(r'^$', ListView.as_view(**page_list_info), name='title-index'),
+    url(r'^(?P<slug>[-\w]+)$', views.PageDetailView.as_view(), name='show-page'),
+    url(r'^(?P<slug>[-\w]+)/edit$', views.PageUpdateView.as_view(), name='edit-page'),
     url(r'^(?P<slug>[-\w]+)/upload', views.upload, name='upload-image'),
-    url(r'^(?P<slug>[-\w]+)/edit$', views.edit, name='edit-page'),
 )
--- sapling/mikepages/views.py
+++ sapling/mikepages/views.py
@@ -1,51 +1,31 @@
-from django.shortcuts import render_to_response
-
 from models import Page
 from forms import PageForm
 from django.views.generic.simple import direct_to_template
-from django.template.defaultfilters import slugify
+from django.views.generic import DetailView, UpdateView
 from ckeditor.views import ck_upload
 
-from django.db.models import AutoField
-from django.http import HttpResponse
-def copy_model_instance(obj):
-    initial = dict([(f.name, getattr(obj, f.name))
-                    for f in obj._meta.fields
-                    if not isinstance(f, AutoField) and\
-                       not f in obj._meta.parents.values()])
-    return obj.__class__(**initial)
+from django.http import HttpResponseNotFound
+from django.core.urlresolvers import reverse
+from utils.views import Custom404Mixin, CreateObjectMixin
 
-def diff(request):
-    pages = Page.objects.all()
-    page1 = pages[0]
-    page2 = pages[1]
-    return render_to_response('mikepages/page_diff.html', {'page1': page1, 'page2': page2})
+class PageDetailView(Custom404Mixin, DetailView):
+    model = Page
+    context_object_name = 'page'
+    
+    def handler404(self, request, slug): 
+        return HttpResponseNotFound(direct_to_template(request, 'mikepages/page_new.html', {'name': slug}))
+    
+
+class PageUpdateView(CreateObjectMixin, UpdateView):
+    model = Page
+    form_class = PageForm
+    
+    def get_success_url(self):
+        return reverse('show-page', args=[self.object.slug])
+    
+    def create_object(self):
+        return Page(name=self.kwargs['slug']) 
+    
 
 def upload(request, slug):
     return ck_upload(request, 'ck_upload/')
-
-def show(request, slug):
-    try:
-        page = Page.objects.get(slug__exact=slugify(slug))
-    except Page.DoesNotExist:
-        return direct_to_template(request, 'mikepages/page_new.html', {'name': slug})
-    return direct_to_template(request, 'mikepages/page_detail.html', {'page': page})
-
-def edit(request, slug):
-    page_old = None
-    try:
-        page = Page.objects.get(slug__exact=slugify(slug))
-        page_old = copy_model_instance(page)
-    except Page.DoesNotExist:
-        page = Page(name=slug)
-    if request.method == 'POST':
-        form = PageForm(request.POST, request.FILES, instance=page)
-        if form.is_valid():
-            form.save()
-            if page_old:
-                return direct_to_template(request, 'mikepages/page_diff.html', 
-                                          {'page1': page_old, 'page2': page})
-            return show(request, page.slug)
-    else:
-        form = PageForm(instance = page)
-    return direct_to_template(request, 'mikepages/page_edit.html', {'form': form})
