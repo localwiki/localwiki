077f52c197b9ee4a8396338bb008ec2d3f88fae4
Mike Ivanov <mivanov@gmail.com>
2011-02-17 00:48:03 -0800

    Slight change for #55.  Hand-coded url is still needed for MEDIA_ROOT, just not for STATIC_ROOT.  Put the media url back.

--- sapling/urls.py
+++ sapling/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +8,10 @@ urlpatterns = patterns('',
     (r'^pages/', include('sapling.pages.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
