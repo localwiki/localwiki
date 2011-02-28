4352f86b436d61464456561ab1ae417f6c9dd737
Philip Neustrom <philipn@gmail.com>
2011-02-28 02:41:08 -0800

    staticfiles should be here

--- sapling/settings.py
+++ sapling/settings.py
@@ -88,8 +88,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-# DONT COMMIT THIS..arggh
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     'philiptest',
 
