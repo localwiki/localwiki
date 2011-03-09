f7346bce1c77e45287b563bc1e5c4b14a6bbdf18
Philip Neustrom <philipn@gmail.com>
2011-03-08 23:17:07 -0800

    Initial geodjango install stuff.

--- sapling/localsettings_template.py
+++ sapling/localsettings_template.py
@@ -1,19 +1,15 @@
-import os
-
 DEBUG = True
 CACHE_BACKEND = 'dummy:///'
 
-DATABASE_ENGINE = 'sqlite3'
-# Or path to database file if using sqlite3.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'dev.db')
-# Not used with sqlite3.
-DATABASE_USER = ''
-# Not used with sqlite3.
-DATABASE_PASSWORD = ''
-# Set to empty string for localhost. Not used with sqlite3.
-DATABASE_HOST = ''
-DATABASE_PORT = ''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'sapling',
+        'USER': 'sapling',
+        'PASSWORD': '** PASSWORD GOES HERE **',
+        'HOST': '127.0.0.1',
+    }
+}
 
 DAISYDIFF_URL = 'http://localhost:8080/diff'
 DAISYDIFF_MERGE_URL = 'http://localhost:8080/merge'
-
--- sapling/settings.py
+++ sapling/settings.py
@@ -12,12 +12,16 @@ MANAGERS = ADMINS
 import os
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -76,20 +80,19 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'sapling.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
     # Django-provided apps
     'django.contrib.auth',
     'django.contrib.admin',
+    'django.contrib.gis',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    
+
     # Other third-party apps
     'sorl.thumbnail',
 
