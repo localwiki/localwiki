f115ccc113ab703f83b5f5157bfb4171d1f1c560
Philip Neustrom <philipn@gmail.com>
2011-03-08 19:09:34 -0800

    Forgot to commit fields.py

---
+++ sapling/versionutils/versioning/fields.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from registry import *
+
+
+class AutoUserField(models.ForeignKey):
+    def __init__(self, **kws):
+        super(AutoUserField, self).__init__(User, **kws)
+
+    def contribute_to_class(self, cls, name):
+        super(AutoUserField, self).contribute_to_class(cls, name)
+        registry = FieldRegistry('user')
+        registry.add_field(cls, self)
+
+
+class AutoIPAddressField(models.IPAddressField):
+    def contribute_to_class(self, cls, name):
+        super(AutoIPAddressField, self).contribute_to_class(cls, name)
+        registry = FieldRegistry('ip')
+        registry.add_field(cls, self)
