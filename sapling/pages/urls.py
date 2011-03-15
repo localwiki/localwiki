86538738dbfcf27cfb395d56953ab350a3f59a57
Mike Ivanov <mivanov@gmail.com>
2011-03-15 15:13:23 -0700

    Fixes #91 Back to simple slugification in urls.py

--- sapling/pages/urls.py
+++ sapling/pages/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic import ListView
 
 import views
+import models
 from models import Page
 
 page_list_info = {
@@ -10,18 +11,28 @@ page_list_info = {
 }
 
 
+def slugify(func):
+    """Applies custom slugify to the slug and stashes original slug
+    """
+    def wrapped(*args, **kwargs):
+        if 'slug' in kwargs:
+            kwargs['original_slug'] = kwargs['slug']
+            kwargs['slug'] = models.slugify(kwargs['slug'])
+        return func(*args, **kwargs)
+    return wrapped
+
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(**page_list_info), name='title-index'),
-    url(r'^(?P<slug>.+)/edit$', views.PageUpdateView.as_view(),
+    url(r'^(?P<slug>.+)/edit$', slugify(views.PageUpdateView.as_view()),
         name='edit-page'),
-    url(r'^(?P<slug>.+)/upload', views.upload, name='upload-image'),
-    url(r'^(?P<slug>.+)/_history/compare$', views.compare),
+    url(r'^(?P<slug>.+)/upload', slugify(views.upload), name='upload-image'),
+    url(r'^(?P<slug>.+)/_history/compare$', slugify(views.compare)),
     url(r'^(?P<slug>.+)/_history/(?P<version1>[0-9]+)...(?P<version2>[0-9]+)$',
-        views.compare, name='compare-revisions'),
+        slugify(views.compare), name='compare-revisions'),
     url(r'^(?P<slug>.+)/_history/(?P<version>[0-9]+)$',
-        views.PageVersionDetailView.as_view(), name='page-version'),
-    url(r'^(?P<slug>.+)/_history/$', views.PageHistoryView.as_view(),
+        slugify(views.PageVersionDetailView.as_view()), name='page-version'),
+    url(r'^(?P<slug>.+)/_history/$', slugify(views.PageHistoryView.as_view()),
         name='page-history'),
-    url(r'^(?P<slug>.+)/$', views.PageDetailView.as_view(),
+    url(r'^(?P<slug>.+)/$', slugify(views.PageDetailView.as_view()),
         name='show-page'),
 )
