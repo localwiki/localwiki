f9c0c47855f397842b1f3f9fad29f3f5aba07937
Philip Neustrom <philipn@gmail.com>
2011-04-11 20:19:19 -0700

    Consistent popup bubble HTML.

--- sapling/versionutils/diff/diffutils.py
+++ sapling/versionutils/diff/diffutils.py
@@ -455,22 +455,22 @@ class GeometryFieldDiff(BaseFieldDiff):
         poly_field2 = _convert_to_multipolygon(poly_field2)
 
         deleted = InfoMap([
-            (poly_same, {'html': '<p>Stayed the same</p>',
+            (poly_same, {'html': 'Stayed the same',
                          'style': {'stroke_opacity': '0'}}),
             (poly_deleted,
-                {'html': '<p>Removed</p>',
+                {'html': 'Removed',
                  'style': {'fill_color': '#ff7777', 'stroke_color': '#ff7777',
                            'fill_opacity': '0.8', 'stroke_opacity': '0'}
                 }
             ),
             (poly_field1.boundary, {}),
             (other_geom_same,
-                {'html': '<p>Stayed the same</p>',
+                {'html': 'Stayed the same',
                  'style': {'fill_color': '#ffdf68', 'stroke_color': '#db9e33',
                            'stroke_opacity': '1'}}
             ),
             (other_geom_deleted,
-                {'html': '<p>Removed</p>',
+                {'html': 'Removed',
                  'style': {'fill_color': '#ff7777', 'stroke_color': '#ff7777',
                            'fill_opacity': '0.8', 'stroke_opacity': '1'}
                 }
@@ -478,22 +478,22 @@ class GeometryFieldDiff(BaseFieldDiff):
         ], options=olwidget_options)
 
         inserted = InfoMap([
-            (poly_same, {'html': '<p>Stayed the same</p>',
+            (poly_same, {'html': 'Stayed the same',
                          'style': {'stroke_opacity': '0'}}),
             (poly_inserted,
-                {'html': '<p>Added</p>',
+                {'html': 'Added',
                  'style': {'fill_color': '#9bff53', 'stroke_color': '#9bff53',
                            'fill_opacity': '0.8', 'stroke_opacity': '0'}
                 }
             ),
             (poly_field2.boundary, {}),
             (other_geom_same,
-                {'html': '<p>Stayed the same</p>',
+                {'html': 'Stayed the same',
                  'style': {'fill_color': '#ffdf68', 'stroke_color': '#db9e33',
                            'stroke_opacity': '1'}}
             ),
             (other_geom_inserted,
-                {'html': '<p>Added</p>',
+                {'html': 'Added',
                  'style': {'fill_color': '#9bff53', 'stroke_color': '#9bff53',
                            'fill_opacity': '0.8', 'stroke_opacity': '1'}
                 }
