e2b14a719c95878663e9b2d840893efd184c2573
Philip Neustrom <philipn@gmail.com>
2011-04-25 18:09:06 -0700

    Boosted name field on page. Almost there.

--- sapling/pages/search_indexes.py
+++ sapling/pages/search_indexes.py
@@ -6,6 +6,8 @@ from models import Page
 
 class PageIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
+    # TODO: We'll likely need to tweak this boost value.
+    name = CharField(model_attr='name', boost=2)
 
 
 site.register(Page, PageIndex)
