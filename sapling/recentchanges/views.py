f6dc1f1be2702ec833a23aa7cfa6c02fbf59d97d
Philip Neustrom <philipn@gmail.com>
2011-04-27 19:10:40 -0700

    HTML/CSS classes, some code organization.

--- sapling/recentchanges/views.py
+++ sapling/recentchanges/views.py
@@ -10,17 +10,20 @@ class RecentChangesView(ListView):
     template_name = "recentchanges/recentchanges.html"
     context_object_name = 'changes_grouped_by_slug'
 
-    def get_queryset(self):
-        mapchanges = list(MapData.history.all())
-        for o in mapchanges:
+    def _format_pages(self, objs):
+        for o in objs:
+            o.classname = 'page'
+        return objs
+
+    def _format_mapdata(self, objs):
+        for o in objs:
             o.slug = o.page.slug
             o.pretty_slug = o.page.pretty_slug
             o.name = o.page.name
-        pagechanges = list(Page.history.all())
+            o.classname = 'map'
+        return objs
 
-        # Merge the two sorted-by-date querysets.
-        objs = merge(pagechanges, mapchanges)
-
+    def _group_by_date_then_slug(self, objs):
         slug_dict = {}
         # objs is currently ordered by date.  Group together by slug.
         for obj in objs:
@@ -35,4 +38,14 @@ class RecentChangesView(ListView):
         # list.
         objs_by_slug = sorted(slug_dict.values(),
                key=lambda x: x[0].history_info.date, reverse=True)
+
         return objs_by_slug
+
+    def get_queryset(self):
+        pagechanges = self._format_pages(Page.history.all())
+        mapchanges = self._format_mapdata(MapData.history.all())
+
+        # Merge the two sorted-by-date querysets.
+        objs = merge(pagechanges, mapchanges)
+
+        return self._group_by_date_then_slug(objs)
