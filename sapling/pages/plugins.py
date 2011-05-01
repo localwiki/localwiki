175439ed3981d47803f14f974968e84d33f05be5
Mike Ivanov <mivanov@gmail.com>
2011-04-30 23:12:57 -0700

    Thumbnails are now links

--- sapling/pages/plugins.py
+++ sapling/pages/plugins.py
@@ -134,6 +134,13 @@ def handle_image(elem, context=None):
         elem.tail = after + (elem.tail or '')
     else:
         elem.attrib['src'] = file.file.url
+    info_url = reverse('pages:file-info', args=[page.pretty_slug,
+                                                    file.name])
+    link = etree.Element('a')
+    link.attrib['href'] = info_url
+    elem.addprevious(link)
+    elem.getparent().remove(elem)
+    link.append(elem)
     return False
 
 
