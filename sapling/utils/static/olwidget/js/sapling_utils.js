f485637be092511728db8a977c49afe7e6d05b04
Mike Ivanov <mivanov@gmail.com>
2011-07-05 18:36:08 -0700

    Fixes #257 Weird poly disappearing issue

--- sapling/utils/static/olwidget/js/sapling_utils.js
+++ sapling/utils/static/olwidget/js/sapling_utils.js
@@ -125,7 +125,7 @@ SaplingMap = {
         var results = $('<ol>');
         var viewedArea = map.getExtent().toGeometry().getArea();
         $.each(layer.features, function(index, feature) {
-            if(feature == selectedFeature)
+           if(feature == selectedFeature)
                 return;
            setAlpha(feature, viewedArea);
            var listResult = false;
@@ -178,7 +178,13 @@ SaplingMap = {
                        new OpenLayers.Projection('EPSG:4326')).toBBOX();
               
         var zoom = map.getZoom();
+        var myDataToken = Math.random();
+        map.dataToken = myDataToken;
         $.get('_objects/', { 'bbox': bbox, 'zoom': zoom }, function(data){
+        	if(map.dataToken != myDataToken)
+        	{
+        		return;
+        	}
             layer.dataExtent = extent;
             var temp = new olwidget.InfoLayer(data);
             temp.visibility = false;
@@ -192,7 +198,7 @@ SaplingMap = {
             $.each(temp.features, function(index, feature) {
               feature.map = map;
               if(selectedFeature && selectedFeature.geometry.toString() == feature.geometry.toString())
-                return;
+                  return;
               layer.addFeatures(feature);
             });
             map.removeLayer(temp);
