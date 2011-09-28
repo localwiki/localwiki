dfd0fba18771ae5d62adab6013a6c61fb08df5c5
Philip Neustrom <philipn@gmail.com>
2011-09-28 13:38:46 -0700

    Fix failing test. Failed b/c contenttype has max of 50 char field names, so model names can't exceed that. 'history' -> 'hist'

--- sapling/versionutils/versioning/models.py
+++ sapling/versionutils/versioning/models.py
@@ -374,7 +374,7 @@ class TrackChanges(object):
             if k in TrackChanges.META_TO_SKIP:
                 continue
             meta[k] = getattr(model._meta, k)
-        meta['verbose_name'] = meta['verbose_name'] + ' history'
+        meta['verbose_name'] = meta['verbose_name'] + ' hist'
         return meta
 
     def post_save(self, parent, instance, created, **kws):
