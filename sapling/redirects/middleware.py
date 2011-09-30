409d887528080fc1323ea29a3c3537bff0b4aba8
Philip Neustrom <philipn@gmail.com>
2011-09-30 16:16:11 -0700

    Force-show URL, prevent double redirect.

--- sapling/redirects/middleware.py
+++ sapling/redirects/middleware.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponsePermanentRedirect
+from django.http import HttpResponseRedirect
 
 from pages.models import slugify
 
 from models import Redirect
+
+
+def _is_redirect(response):
+    return 'redirected_from' in response.GET
+
+
+def _force_show_page(response):
+    return 'force_show' in response.GET
 
 
 class RedirectFallbackMiddleware(object):
@@ -10,21 +18,26 @@ class RedirectFallbackMiddleware(object):
         if response.status_code != 404:
             # No need to check for a redirect for non-404 responses.
             return response
+        if _is_redirect(request) or _force_show_page(request):
+            # Don't double-redirect and allow the page to be
+            # force-displayed.
+            return response
+
         r = None
         # Skip leading slash.
         slug = slugify(request.get_full_path()[1:])
+        # Skip trailing slash.
+        if slug.endswith('/'):
+            slug = slug[:-1]
         try:
             r = Redirect.objects.get(source=slug)
         except Redirect.DoesNotExist:
             pass
-        # Try removing the trailing slash, if it exists.
-        if r is None and slug.endswith('/'):
-            try:
-                r = Redirect.objects.get(source=slug[:-1])
-            except Redirect.DoesNotExist:
-                pass
         if r is not None:
-            return HttpResponsePermanentRedirect(r.destination.get_absolute_url())
+            return HttpResponseRedirect(
+                r.destination.get_absolute_url() +
+                '?&redirected_from=%s' % slug
+            )
 
         # No redirect was found. Return the response.
         return response
