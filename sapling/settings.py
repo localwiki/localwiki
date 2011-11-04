8e01c747886a88dcf3fc87a2f64e1a78e52dcdaa
Philip Neustrom <philipn@gmail.com>
2011-11-04 14:47:00 -0700

    Fixes DeprecationWarning.

--- sapling/settings.py
+++ sapling/settings.py
@@ -132,8 +132,8 @@ CLOUDMADE_API = '%solwidget/js/sapling_cloudmade.js?tm=1317359250' % STATIC_URL
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
