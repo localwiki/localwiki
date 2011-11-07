edb40cfe2d5e0bfcbf66bd8ac72b0dfb1ea9aa44
Mike Ivanov <mivanov@gmail.com>
2011-11-07 10:06:30 -0800

    Changed package name to localwiki, suite to stable

--- deb_utils/makedeb.sh
+++ deb_utils/makedeb.sh
@@ -4,7 +4,7 @@ rm -r deb_dist/
 rm -r localwiki.egg-info/
 python setup.py --command-packages=stdeb.command sdist_dsc --ignore-install-requires
 cd deb_utils
-cp python-localwiki.postinst ../deb_dist/localwiki-$version/debian
+cp localwiki.postinst ../deb_dist/localwiki-$version/debian
 cp triggers ../deb_dist/localwiki-$version/debian
 cd ../deb_dist/localwiki-$version
 dpkg-buildpackage -rfakeroot -uc -us
