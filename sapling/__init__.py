1066bddd2c675498cf8815707acc635d9233fdc6
Mike Ivanov <mivanov@gmail.com>
2011-11-21 15:07:24 -0800

    Version to beta 1

--- sapling/__init__.py
+++ sapling/__init__.py
@@ -1,13 +1,11 @@
-VERSION = (0, 1, 2, 'alpha', 0)
+VERSION = (0, 2, 'beta', 1)
 
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
+    if VERSION[2:] == ('alpha', 0):
         version = '%s pre-alpha' % version
     else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+        if VERSION[2] != 'final':
+            version = '%s %s %s' % (version, VERSION[2], VERSION[3])
     return version
