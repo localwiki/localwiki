884976241c094bbc08c3d2f546026b832a65810e
Philip Neustrom <philipn@gmail.com>
2011-11-27 16:04:36 -0800

    Load data in setup_all. Changelog.

--- sapling/utils/management/commands/setup_all.py
+++ sapling/utils/management/commands/setup_all.py
@@ -2,6 +2,11 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 
+def has_pages():
+    from pages.models import Page
+    return Page.objects.all()
+
+
 class Command(BaseCommand):
     help = ('Sets up the localwiki install.  '
             'Only run this command on a fresh install.')
@@ -11,3 +16,7 @@ class Command(BaseCommand):
         call_command('migrate', verbosity=0)
         call_command('collectstatic', interactive=False, verbosity=0)
         call_command('reset_permissions', verbosity=0)
+
+        if not has_pages():
+            print "Adding basic pages.."
+            call_command('loaddata', 'basic_pages')
