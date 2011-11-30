20d89e927560272ba453c18c3e9bd35e18076a26
Philip Neustrom <philipn@gmail.com>
2011-11-29 22:49:27 -0800

    Fixed template for Ubuntu 10.11. Exec each indepen.

--- sapling/etc/install_config/postgis_template_scripts/create_template_postgis-debian.sh
+++ sapling/etc/install_config/postgis_template_scripts/create_template_postgis-debian.sh
@@ -14,22 +14,30 @@ then
     POSTGIS_SQL=postgis.sql
 fi
 
-# For Ubuntu 10.10 (with PostGIS 1.5)
+# For Ubuntu 10.10 and Ubuntu 11.04 (with PostGIS 1.5)
 if [ -d "/usr/share/postgresql/8.4/contrib/postgis-1.5" ]
 then
     POSTGIS_SQL_PATH=/usr/share/postgresql/8.4/contrib/postgis-1.5
+    POSTGIS_SQL=postgis.sql
+    GEOGRAPHY=1
+fi
+
+# For Ubuntu 11.10 (with PostGIS 1.5)
+if [ -d "/usr/share/postgresql/9.1/contrib/postgis-1.5" ]
+then
+    POSTGIS_SQL_PATH=/usr/share/postgresql/9.1/contrib/postgis-1.5
     POSTGIS_SQL=postgis.sql
     GEOGRAPHY=1
 else
     GEOGRAPHY=0
 fi
 
-createdb -E UTF8 template_postgis && \
-createlang -d template_postgis plpgsql && \
-psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" && \
-psql -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL && \
-psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql && \
-psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" && \
+createdb -E UTF8 template_postgis
+createlang -d template_postgis plpgsql
+psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
+psql -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL
+psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
+psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
 psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
 if ((GEOGRAPHY))
