3f1f941a087c0147176fe9bf74a1ed02809d3f17
Philip Neustrom <philipn@gmail.com>
2011-11-30 00:32:12 -0800

    Test explicitly to see if template exists.
    We test explicitly to avoid errors when installing w/ an already existing template_postgis on 11.10

--- sapling/etc/install_config/postgis_template_scripts/create_template_postgis-debian.sh
+++ sapling/etc/install_config/postgis_template_scripts/create_template_postgis-debian.sh
@@ -33,14 +33,22 @@ else
 fi
 
 createdb -E UTF8 template_postgis
-createlang -d template_postgis plpgsql
-psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
-psql -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL
-psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
-psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
-psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
-
-if ((GEOGRAPHY))
+OUT=$?
+# template doesn't exist yet, so let's create
+if [ $OUT -eq 0 ]
 then
-    psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+   createlang -d template_postgis plpgsql
+   psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
+   psql -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL
+   psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
+   psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
+   psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+
+   if ((GEOGRAPHY))
+   then
+       psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+   fi
+
+else
+   exit
 fi
