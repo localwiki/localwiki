74fac18188f388074a79b75e290030e18c3d1dac
Mike Ivanov <mivanov@gmail.com>
2011-12-07 11:47:33 -0800

    Fixes #5 dragging images

--- sapling/pages/static/js/ckeditor/_source/plugins/simpleimage/plugin.js
+++ sapling/pages/static/js/ckeditor/_source/plugins/simpleimage/plugin.js
@@ -113,44 +113,84 @@ CKEDITOR.plugins.add( 'simpleimage',
 					jQuery(window).resize();
 				});
 			});
-		   	jQuery(editor.document.$.body).bind('dragstart', function(evt){
-		   		editor.fire('saveSnapshot');
-		   		var img = jQuery(evt.target);
-	   			if(!img.is('img'))
-	   				return;
-	   			var oldFrame = img.parent('span.image_frame');
-	   			var oldHtml = oldFrame.length ? oldFrame.outerHTML() : img.outerHTML();
-	   			img.attr('dragged', '1');
-	   			var moveImage = function(evt){
-	   				var dropped = jQuery('img[dragged="1"]', editor.document.$);
-	   				if(dropped.length)
-	   				{
-	   					if(dropped[0] != img[0])
-	   						oldFrame.remove();
-	   					// FF seems to copy frame span also
-	   					var frame = dropped.parent('span.image_frame');
-	   					if(frame.length)
-	   						dropped = frame;
-	   					dropped.outerHTML(oldHtml);
-	   				}
-	   				jQuery(this).unbind('dragend');
-	   			};
-	   			jQuery(evt.target).bind('dragend', moveImage)
-	   					 .parent().bind('dragend', moveImage);
-	   		});
-		    
-		    // delete image -> delete its parent frame
-		    editor.document.on( 'keydown', function( evt )
+            jQuery(editor.document.$.body).bind('dragstart', function(evt){
+                editor.fire('saveSnapshot');
+                var savedImages = {};
+                jQuery('span.image_frame', editor.document.$).each(function(index){
+                   savedImages[index] = jQuery(this).html();
+                   jQuery(this).attr('cke_saved_id', index); 
+                });
+                var restoreImages = function(){
+                    jQuery('span.image_frame', editor.document.$).each(function(index){
+                        var me = jQuery(this);
+                        var saved_id = me.attr('cke_saved_id');
+                        if(savedImages[saved_id])
+                        {
+                            me.html(savedImages[saved_id]);
+                            delete savedImages[saved_id];
+                        }
+                        else me.remove();
+                    }).removeAttr('cke_saved_id');
+                };
+            	var img = jQuery(evt.target);
+            	if(!img.is('img'))
+            		return;
+            	var oldFrame = img.parent('span.image_frame');
+            	var oldHtml = oldFrame.length ? oldFrame.outerHTML() : img.outerHTML();
+            	img.addClass('cke_moved');
+            	oldFrame.addClass('cke_moved');
+            	var moveImage = function(evt){
+                    oldFrame.remove();
+            		var moved_image = jQuery('img.cke_moved', editor.document.$);
+            		if(moved_image.length == 0)
+            		{
+            		    restoreImages();
+            		    return;
+            		}
+            		var moved_frame = jQuery('span.cke_moved', editor.document.$);
+            		var moved_element = moved_frame.length ? moved_frame : moved_image;
+            		var outerFrame = moved_element.parent().closest('span.image_frame');
+            		if(outerFrame.length)
+            		{
+            		    // was dropped inside another frame
+            		    var images = jQuery('img', outerFrame[0]);
+            		    // dropped before image or after?
+            		    if(images.length && images[0] == moved_image[0])
+            		        outerFrame.before(oldHtml);
+            		    else outerFrame.after(oldHtml);
+            		} else {
+            		    moved_element.before(oldHtml);
+            		}
+            		// fix the cursor position
+            		var selection = editor.getSelection();
+            		var range = selection.getRanges()[0];
+            		var ckelement = new CKEDITOR.dom.element(moved_element[0]);
+            		ckelement.getParent().appendBogus();
+            		range.setStartBefore(ckelement);
+            		range.setEndBefore(ckelement);
+            		selection.selectRanges([range]);
+            		moved_image.remove();
+            		moved_frame.remove();
+            		restoreImages();
+            		jQuery(window).resize();
+            		return false;
+            	};
+            	jQuery(evt.target).one('dragend', moveImage)
+            			 .parent().one('dragend', moveImage);
+            });
+            
+            // delete image -> delete its parent frame
+            editor.document.on( 'keydown', function( evt )
             {
                 var keyCode = evt.data.getKeystroke();
                 // Backspace OR Delete.
                 if ( keyCode in { 8 : 1, 46 : 1 } )
                 {
 					var sel = editor.getSelection(),
-						element = sel.getSelectedElement();
+						element = sel.getStartElement();
                     if ( element && element.is('img'))
                     {
-                        element = element.getAscendant('span');
+                        element = element.getAscendant('span', true);
                         if(!element)
                             return;
                         // Make undo snapshot.
