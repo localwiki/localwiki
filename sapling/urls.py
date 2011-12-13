3b1656e13871a2519fc072f7f282d4e6af743b2c
Philip Neustrom <philipn@gmail.com>
2011-12-12 21:59:00 -0800

    Allow visiting /admin in addition to /admin/ for easy of discovery.

--- sapling/urls.py
+++ sapling/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^(?i)Users/', include('sapling.users.urls')),
     (r'^search/', include('sapling.search.urls')),
     (r'^', include('sapling.recentchanges.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/*', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # This should only happen if you're using the local dev server with
