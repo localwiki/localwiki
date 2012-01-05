c1c8fda5897b64bcb53b4802c968257b5ce9a86a
Philip Neustrom <philipn@gmail.com>
2012-01-05 12:25:38 -0800

    Remove accidential forms URL commit.

--- sapling/urls.py
+++ sapling/urls.py
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
     (r'^(?i)Users/', include('sapling.users.urls')),
     (r'^search/', include('sapling.search.urls')),
     (r'^', include('sapling.recentchanges.urls')),
-    (r'^forms', include('forms_builder.forms.urls')),
     (r'^admin/*', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
