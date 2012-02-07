02d950ec90ff9ebaaa9ccb2d8391e07a5e5942ac
Philip Neustrom <philipn@gmail.com>
2012-02-06 22:16:14 -0800

    Remove extra JSONResponseMixin, re-org.

--- sapling/utils/views.py
+++ sapling/utils/views.py
@@ -53,6 +53,16 @@ class JSONResponseMixin(object):
         return json.dumps(context)
 
 
+class JSONView(View, JSONResponseMixin):
+    """
+    A JSONView returns, on GET, a json dictionary containing the values of
+    get_context_data().
+    """
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+
 class PermissionRequiredMixin(object):
     """
     View mixin for verifying permissions before updating an existing object
@@ -110,31 +120,4 @@ class PermissionRequiredMixin(object):
                                                         **kwargs)
 
 
-class JSONResponseMixin(object):
-    def render_to_response(self, context):
-        "Returns a JSON response containing 'context' as payload"
-        return self.get_json_response(self.convert_context_to_json(context))
-
-    def get_json_response(self, content, **httpresponse_kwargs):
-        "Construct an `HttpResponse` object."
-        return HttpResponse(content, content_type='application/json',
-            **httpresponse_kwargs)
-
-    def convert_context_to_json(self, context):
-        "Convert the context dictionary into a JSON object"
-        # Note: This is *EXTREMELY* naive; in reality, you'll need
-        # to do much more complex handling to ensure that arbitrary
-        # objects -- such as Django model instances or querysets
-        # -- can be serialized as JSON.
-        return json.dumps(context)
-
-
-class JSONView(View, JSONResponseMixin):
-    """
-    A JSONView returns, on GET, a json dictionary containing the values of
-    get_context_data().
-    """
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
 
