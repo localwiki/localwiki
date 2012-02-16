682257e64ccbda094764b8f221521f2d73362b76
Nuno Maltez <nuno@cognitiva.com>
2012-02-16 16:13:42 +0000

    i18n - users

--- sapling/users/__init__.py
+++ sapling/users/__init__.py
@@ -1,10 +1,10 @@
 from django.contrib import auth
 from django.contrib import messages
-
+from django.utils.translation import ugettext as _
 
 def logged_out_msg(sender, request, user, **kwargs):
     messages.add_message(request, messages.SUCCESS,
-        "You are now logged out.")
+        _("You are now logged out."))
 
 auth.signals.user_logged_out.connect(logged_out_msg,
     dispatch_uid='user_logged_out_msg')
