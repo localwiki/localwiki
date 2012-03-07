e02c780200206594754f5326c187b200ae9a4288
Philip Neustrom <philipn@gmail.com>
2012-03-06 19:35:22 -0800

    MultiPolygon checks + intersects() fixes.
    We use intersects() rather than containsPoint() because MultiPolygons don't have containsPoint() and iterating would be messy.
    
    We send back Multi* geometries via the API, hence the changes.

--- sapling/maps/static/olwidget/js/sapling_utils.js
+++ sapling/maps/static/olwidget/js/sapling_utils.js
@@ -132,7 +132,8 @@ SaplingMap = {
             });
         };
         var setAlpha = function(feature, viewedArea) {
-            if(feature.geometry.CLASS_NAME == "OpenLayers.Geometry.Polygon")
+            if(feature.geometry.CLASS_NAME == "OpenLayers.Geometry.Polygon" ||
+               feature.geometry.CLASS_NAME == "OpenLayers.Geometry.MultiPolygon")
             {
                 var alpha =  0.5 - 0.5 * Math.min(1, feature.geometry.getArea()/viewedArea);
                 var polyStyle = $.extend({}, 
@@ -154,11 +155,12 @@ SaplingMap = {
            var listResult = false;
            if(selectedFeature)
            {
-               if(selectedFeature.geometry.CLASS_NAME == "OpenLayers.Geometry.Polygon")
+               if(selectedFeature.geometry.CLASS_NAME == "OpenLayers.Geometry.Polygon" ||
+                  selectedFeature.geometry.CLASS_NAME == "OpenLayers.Geometry.MultiPolygon")
                {
                    header = 'Things inside ' + selectedFeature.attributes.html + ':';
                    $.each(feature.geometry.getVertices(), function(ind, vertex){
-                       if(selectedFeature.geometry.containsPoint(vertex))
+                       if(selectedFeature.geometry.intersects(vertex))
                        {
                            listResult = true;
                            return false;
@@ -203,60 +205,69 @@ SaplingMap = {
                        new OpenLayers.Projection('EPSG:4326')).toGeometry());
 
         var zoom = map.getZoom();
+        var min_length = 100 * Math.pow(2, 0 - zoom);
         var myDataToken = Math.random();
         layer.dataToken = myDataToken;
 
-        var min_length = 100 * Math.pow(2, 0 - zoom);
-
         var page_endpoint = SaplingMap.api_endpoint + 'page/';
 
-        var add_geom_items = function(data) {
-            var geom_items = [];
+        var process_geom_data = function (geoms) {
+            if(layer.dataToken != myDataToken)
+            {
+                return;
+            }
+            layer.dataExtent = extent;
+            var temp = new olwidget.InfoLayer(geoms);
+            temp.visibility = false;
+            map.addLayer(temp);
+            layer.removeAllFeatures();
+            if(selectedFeature)
+            {
+              layer.addFeatures(selectedFeature);
+              layer.selectedFeatures = [selectedFeature];
+            }
+            $.each(temp.features, function(index, feature) {
+              feature.map = map;
+              if(selectedFeature && selectedFeature.geometry.toString() == feature.geometry.toString())
+                  return;
+              layer.addFeatures(feature);
+            });
+            map.removeLayer(temp);
+            if(callback){
+                callback();
+            }
+        }
+
+        var add_geom_items = function (data, geom_type, geom_items) {
             // Convert returned GeoJSON into WKT for olWidget
             for (var i=0; i<data.objects.length; i++) {
                 var item = data.objects[i];
                 var page_url = item.page.slice(page_endpoint.length - 1);
                 var page_name = decodeURIComponent(page_url.slice(1)).replace('_', ' ');
                 var geom_html = '<a href="' + page_url + '">' + page_name + '</a>';
-                var geom_wkt = WKT.extractGeometry(geoJSON.read(item.geom, 'Geometry'));
+                var geom_wkt = WKT.extractGeometry(geoJSON.read(item[geom_type], 'Geometry'));
                 geom_items.push([geom_wkt, geom_html]);
             }
-
-            var temp = new olwidget.InfoLayer(geom_items);
-            temp.visibility = false;
-            map.addLayer(temp);
-
-            $.each(temp.features, function(index, feature) {
-              feature.map = map;
-              if(selectedFeature && selectedFeature.geometry.toString() == feature.geometry.toString()) {
-                  return;
-              }
-              layer.addFeatures(feature);
-            });
-            map.removeLayer(temp);
-        };
-
-        if(layer.dataToken != myDataToken) {
-            return;
-        }
-        layer.dataExtent = extent;
-        layer.removeAllFeatures();
-        if(selectedFeature) {
-            layer.addFeatures(selectedFeature);
-            layer.selectedFeatures = [selectedFeature];
         }
 
         var geom_items = [];
-        /* TODO: make this one request by adding OR support to tastypie? */
-        $.getJSON(SaplingMap.api_endpoint + 'map/', {'polys__within': bbox, 'length__gte': min_length}, add_geom_items);
-        $.getJSON(SaplingMap.api_endpoint + 'map/', {'lines__within': bbox, 'length__gte': min_length}, add_geom_items);
-        if (zoom >= 14) {
-            $.getJSON(SaplingMap.api_endpoint + 'map/', {'points__within': bbox}, add_geom_items);
-        }
 
-        if(callback) {
-            callback();
-        }
+        /* TODO: make this one request by adding OR support to tastypie? */
+        $.getJSON(SaplingMap.api_endpoint + 'map/', {'polys__within': bbox, 'length__gte': min_length}, function (data) {
+            add_geom_items(data, 'polys', geom_items); 
+            $.getJSON(SaplingMap.api_endpoint + 'map/', {'lines__within': bbox, 'length__gte': min_length}, function (data) {
+                add_geom_items(data, 'lines', geom_items); 
+                if (zoom >= 14) {
+                    $.getJSON(SaplingMap.api_endpoint + 'map/', {'points__within': bbox}, function (data) {
+                        add_geom_items(data, 'points', geom_items); 
+                        process_geom_data(geom_items);
+                    });
+                }
+                else {
+                    process_geom_data(geom_items);
+                }
+            });
+        });
     },
 
     _registerEvents: function(map, layer) {
