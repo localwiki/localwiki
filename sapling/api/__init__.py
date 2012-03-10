ccc0cbf04efa759d20b3da3b01a79dae4fb83aad
Philip Neustrom <philipn@gmail.com>
2012-03-10 12:25:27 -0800

    More experimenting w/ history.
    Got the main /_history URL working and the /page/<slug>/_history URL working.  But the /page/<slug>/_history view is screwed up b/c it should expect to return a list, not a single instance.  And we need another URL item for /page/<slug>/_history/<version_number>

--- sapling/api/__init__.py
+++ sapling/api/__init__.py
@@ -49,20 +49,37 @@ class SlugifyMixin(object):
                 # Our slug field.
                 # Slugs can't start with the _ character. We do this so we can
                 # define URLs for sub-resources more easily.
-                url(r"^(?P<parent_resource_name>%s)/(?P<%s>[^_].*?)/(?P<subresource_name>%s)$" %
+                url(r"^(?P<parent_resource_name>%s)/(?P<%s>[^_].*?)/(?P<subresource_name>%s)/*$" %
                     (self._meta.parent_resource._meta.resource_name, slug, self._meta.subresource_name),
                     self.wrap_view('dispatch_detail'), name="api_dispatch_detail")
             )
         else:
             l.append(
                 # Our slug field.
-                # Slugs can't start with the _ character. We do this so we can
+                # Slugs can't start with the _ character or contain a
+                # slash surrounded by the _ character. We do this so we can
                 # define URLs for sub-resources more easily.
-                url(r"^(?P<resource_name>%s)/(?P<%s>[^_].*?)/*$" %
+                url(r"^(?P<resource_name>%s)/(?P<%s>[^_]+((?!(/_)|(_/)).)*?)/*$" %
                     (self._meta.resource_name, slug),
                     self.wrap_view('dispatch_detail'), name="api_dispatch_detail")
             )
         return l
+
+    def remove_api_resource_names(self, url_dict):
+        """
+        We added two new URL keywords -- ``parent_resource_name`` and
+        ``subresource_name``, so we remove them from the kwargs as well.
+        """
+        url_dict = super(SlugifyMixin, self).remove_api_resource_names(url_dict)
+        kwargs_subset = url_dict.copy()
+
+        for key in ['parent_resource_name', 'subresource_name']:
+            try:
+                del(kwargs_subset[key])
+            except KeyError:
+                pass
+
+        return kwargs_subset
 
     def get_resource_uri(self, bundle_or_obj):
         if hasattr(self._meta, 'parent_resource'):
