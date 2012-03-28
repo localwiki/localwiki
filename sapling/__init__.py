a81a0e056de4a3b81d19ebfa2869727593e94302
Mike Ivanov <mivanov@gmail.com>
2012-03-27 19:38:03 -0700

    Version to 0.3.0, simple version scheme

--- sapling/__init__.py
+++ sapling/__init__.py
@@ -1,11 +1,5 @@
-VERSION = (0, 2, 'beta', 16)
+VERSION = (0, 3, 0)
 
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[2] != 'final':
-            version = '%s %s %s' % (version, VERSION[2], VERSION[3])
-    return version
+    return '%s.%s.%s' % VERSION
