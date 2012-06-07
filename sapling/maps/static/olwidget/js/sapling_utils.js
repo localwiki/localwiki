4eea02ca1444014a289f2967ea7f2e1dd9196154
Philip Neustrom <philipn@gmail.com>
2012-06-06 20:11:11 -0700

    Warn that map editing doesn't work in IE 8/9.
    Temporary until #286 is resolved.

--- sapling/maps/static/olwidget/js/sapling_utils.js
+++ sapling/maps/static/olwidget/js/sapling_utils.js
@@ -325,6 +325,10 @@ SaplingMap = {
         for (var i = 0; i < map.controls.length; i++) { 
             if (map.controls[i] && map.controls[i].CLASS_NAME == 
         "olwidget.EditableLayerSwitcher") { 
+                /* Temporary until issue #286 is fixed */
+                if ( $.browser.msie && (parseInt($.browser.version, 10) >= 8) ) {
+                    alert("At the moment, map editing doesn't work in IE 8 & 9. Please use Firefox or Chrome instead to edit maps.");
+                }
                 layer = map.vectorLayers[0];
                 if (layer.controls) {
                     this._remove_unneeded_controls(layer);
