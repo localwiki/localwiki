6f78c8dad35f229b312ba7f3a6ed5a6490242a7b
Philip Neustrom <philipn@gmail.com>
2012-06-08 14:00:13 -0700

    No IE8/9 warning for normal page views w/ map.

--- sapling/maps/static/olwidget/js/sapling_utils.js
+++ sapling/maps/static/olwidget/js/sapling_utils.js
@@ -325,12 +325,12 @@ SaplingMap = {
         for (var i = 0; i < map.controls.length; i++) { 
             if (map.controls[i] && map.controls[i].CLASS_NAME == 
         "olwidget.EditableLayerSwitcher") { 
-                /* Temporary until issue #286 is fixed */
-                if ( $.browser.msie && (parseInt($.browser.version, 10) >= 8) ) {
-                    alert("At the moment, map editing doesn't work in IE 8 & 9. Please use Firefox or Chrome instead to edit maps.");
-                }
                 layer = map.vectorLayers[0];
                 if (layer.controls) {
+                    /* Temporary until issue #286 is fixed */
+                    if ( $.browser.msie && (parseInt($.browser.version, 10) >= 8) ) {
+                        alert("At the moment, map editing doesn't work in IE 8 & 9. Please use Firefox or Chrome instead to edit maps.");
+                    }
                     this._remove_unneeded_controls(layer);
                     map.controls[i].setEditing(layer);
 
