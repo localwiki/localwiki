d9299a41c67dffb3b54621ab9ed6743a5e454c10
Niran Babalola <niran@niran.org>
2012-09-27 00:27:33 -0500

    Add default values from localsettings.py that aren't placeholders

--- sapling/settings/defaults.py
+++ sapling/settings/defaults.py
@@ -267,7 +267,44 @@ INSTALLED_APPS = (
 )
 
 LOCAL_INSTALLED_APPS = ()
-# We set this manually, after localsettings import, below.
 TEMPLATE_DIRS = ()
 
 SITE_THEME = 'sapling'
+
+# For testing, you can start the python debugging smtp server like so:
+# sudo python -m smtpd -n -c DebuggingServer localhost:25
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+
+#######################################################################
+# Other config values.
+#######################################################################
+
+OLWIDGET_DEFAULT_OPTIONS = {
+    'default_lat': 37.76,
+    'default_lon': -122.43,
+    'default_zoom': 12,
+    'zoom_to_data_extent_min': 16,
+
+    'layers': ['cloudmade.35165', 've.aerial'],
+    'map_options': {
+        'controls': ['Navigation', 'PanZoomBar', 'KeyboardDefaults'],
+        'theme': '/static/openlayers/theme/sapling/style.css',
+    },
+    'overlay_style': {'fillColor': '#ffc868',
+                      'strokeColor': '#db9e33',
+                      'strokeDashstyle': 'solid'},
+    'map_div_class': 'mapwidget',
+}
+
+DAISYDIFF_URL = 'http://localhost:8080/daisydiff/diff'
+DAISYDIFF_MERGE_URL = 'http://localhost:8080/daisydiff/merge'
+
+# list of regular expressions for white listing embedded URLs
+EMBED_ALLOWED_SRC = ['.*']
+
+HAYSTACK_SOLR_URL = 'http://localhost:8080/solr'
+
+CACHE_BACKEND = 'dummy:///'
