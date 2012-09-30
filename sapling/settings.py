f865560cf16d9dabaccf4f34e532acd053be3193
Philip Neustrom <philipn@gmail.com>
2012-09-29 20:24:30 -0700

    OpenLayers 2.11. Fixes missing permalink + mobile.
    Some basic mobile support for interacting with the map and for basic editing. Modifying features on mobile is awkward but it looks like there's work being done on that. Could use some dedicated TLC - but mobile stuff just came free with the version bump.

--- sapling/settings.py
+++ sapling/settings.py
@@ -164,7 +164,7 @@ HAYSTACK_SEARCH_ENGINE = 'solr'
 
 THUMBNAIL_BACKEND = 'utils.sorl_backends.AutoFormatBackend'
 
-OL_API = STATIC_URL + 'openlayers/OpenLayers.js?tm=1330130829'
+OL_API = STATIC_URL + 'openlayers/OpenLayers.js?tm=1348975452'
 OLWIDGET_CSS = '%solwidget/css/sapling.css?tm=1317359250' % STATIC_URL
 OLWIDGET_JS = '%solwidget/js/olwidget.js?tm=1317359250' % STATIC_URL
 CLOUDMADE_API = '%solwidget/js/sapling_cloudmade.js?tm=1317359250' % STATIC_URL
