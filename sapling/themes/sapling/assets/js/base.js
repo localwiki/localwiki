3cec0f8c32d1a3939fc98395b181398b8318a6c9
Philip Neustrom <philipn@gmail.com>
2012-12-30 17:27:52 -0800

    Fixes #407.

--- sapling/themes/sapling/assets/js/base.js
+++ sapling/themes/sapling/assets/js/base.js
@@ -44,11 +44,11 @@ $(function() {
         e.preventDefault();  
         menu.slideToggle();  
     });  
-});
 
-$(window).resize(function(){  
-    var w = $(window).width();  
-    if(w > 320 && menu.is(':hidden')) {  
-        menu.removeAttr('style');  
-    }  
+    $(window).resize(function(){  
+        var w = $(window).width();  
+        if(w > 320 && menu.is(':hidden')) {  
+            menu.removeAttr('style');  
+        }  
+    });
 });
