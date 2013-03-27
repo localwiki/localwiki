c92e4d7474abd01226f35276a9c512fd19fbd984
Philip Neustrom <philipn@gmail.com>
2013-03-26 17:55:36 -0700

    +12.10 for ppastats.py. Paging @ivanov - the dates look bad now for some reason

--- deb_utils/ppastats.py
+++ deb_utils/ppastats.py
@@ -14,7 +14,7 @@ import os
 from launchpadlib.launchpad import Launchpad
 
 archs = ["i386"]
-releases = ["lucid", "maverick", "natty", "oneiric", "precise"]
+releases = ["lucid", "maverick", "natty", "oneiric", "precise", "quantal"]
 releases.reverse()
 
 if len(sys.argv) < 3:
