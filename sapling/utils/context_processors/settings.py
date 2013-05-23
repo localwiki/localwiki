f0e5f52e2fe34a774460a50d4d095cb5f31e2848
groovecoder <luke.crouch@gmail.com>
2013-05-22 23:21:22 -0500

    fix #513 - move GOOGLE_ANALYTICS_ID into constance

--- sapling/utils/context_processors/settings.py
+++ sapling/utils/context_processors/settings.py
@@ -7,11 +7,3 @@ def license_agreements(context):
         'EDIT_LICENSE_NOTE': settings.EDIT_LICENSE_NOTE,
         'SIGNUP_TOS': settings.SIGNUP_TOS,
     }
-
-
-def services(context):
-    return {
-        'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', ''),
-        'GOOGLE_ANALYTICS_SUBDOMAINS': getattr(settings, 'GOOGLE_ANALYTICS_SUBDOMAINS', ''),
-        'GOOGLE_ANALYTICS_MULTIPLE_TOPLEVEL_DOMAINS': getattr(settings, 'GOOGLE_ANALYTICS_MULTIPLE_TOPLEVEL_DOMAINS', False)
-    }
