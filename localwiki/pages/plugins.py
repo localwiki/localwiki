cf0e6a6a996cbb396db9bb25b97571d25618a4e2
Philip Neustrom <philipn@gmail.com>
2013-08-09 20:01:12 -0700

    Fixed page links.

--- localwiki/pages/plugins.py
+++ localwiki/pages/plugins.py
@@ -322,6 +322,7 @@ class LinkNode(Node):
         self.nodelist = nodelist
 
     def render(self, context):
+        region = context['region']
         try:
             cls = ''
             url = self.href
@@ -329,11 +330,11 @@ class LinkNode(Node):
             if self.is_relative_link(url):
                 if url.startswith('_files/'):
                     filename = file_url_to_name(url)
-                    url = reverse('pages:file-info', args=[page.pretty_slug,
-                                                       filename])
+                    url = reverse('pages:file-info',
+                            args=[region.slug, page.pretty_slug, filename])
                     try:
-                        file = PageFile.objects.get(slug__exact=page.slug,
-                                                    name__exact=filename)
+                        file = PageFile.objects.get(
+                            slug__exact=page.slug, region=region, name__exact=filename)
                         cls = ' class="file_%s"' % file.rough_type
                     except PageFile.DoesNotExist:
                         cls = ' class="missing_link"'
@@ -341,15 +342,15 @@ class LinkNode(Node):
                     cls = ' class="tag_link"'
                 else:
                     try:
-                        page = Page.objects.get(slug__exact=slugify(url))
-                        url = reverse('pages:show', args=[page.pretty_slug])
+                        page = Page.objects.get(slug__exact=slugify(url), region=region)
+                        url = reverse('pages:show', args=[region.slug, page.pretty_slug])
                     except Page.DoesNotExist:
                         # Check if Redirect exists.
-                        if not Redirect.objects.filter(source=slugify(url)):
+                        if not Redirect.objects.filter(source=slugify(url), region=region):
                             cls = ' class="missing_link"'
                         # Convert to proper URL: My%20page -> My_page
                         url = name_to_url(url_to_name(url))
-                        url = reverse('pages:show', args=[url])
+                        url = reverse('pages:show', args=[region.slug, url])
             return '<a href="%s"%s>%s</a>' % (url, cls,
                                               self.nodelist.render(context))
         except:
