72b7ed2c9cebcb916309899d514477120a499b9f
Philip Neustrom <philipn@gmail.com>
2013-08-11 01:17:01 -0700

    Some crummy user page routing logic.
    Fixes #609.

--- localwiki/users/views.py
+++ localwiki/users/views.py
@@ -13,10 +13,17 @@ class UserPageView(RedirectView):
     def get_redirect_url(self, **kwargs):
         username = self.kwargs.get('username')
         user = User.objects.get(username=username)
-        user_pages = Page.objects.filter(name=username)
+        pagename = "Users/%s" % username
+        user_pages = Page.objects.filter(name=pagename)
         if user_pages:
             # Has a userpage in a region
             user_page = user_pages[0]
         else:
-            user_page = Page(name=username, region=get_main_region())
+            # Check to see if they've edited a region recently
+            edited_pages = Page.versions.filter(version_info__user=user)
+            if edited_pages:
+                region = edited_pages[0].region
+                user_page = Page(name=pagename, region=region)
+            else:
+                user_page = Page(name=pagename, region=get_main_region())
         return user_page.get_absolute_url()
