aa73c315d029b5ac6bbaa4ddf8251a5269ca279d
Philip Neustrom <philipn@gmail.com>
2013-12-07 13:35:16 -0800

    GeoJSON in /api/regions

--- localwiki/regions/serializers.py
+++ localwiki/regions/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
+from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework_gis.serializers import GeoModelSerializer
 
 from .models import Region, RegionSettings
 
 
-class RegionSettingsSerializer(serializers.HyperlinkedModelSerializer):
+class RegionSettingsSerializer(GeoModelSerializer, HyperlinkedModelSerializer):
     class Meta:
         model = RegionSettings
         fields = ('region_center', 'region_zoom_level', 'default_language')
 
 
-class RegionSerializer(serializers.HyperlinkedModelSerializer):
+class RegionSerializer(GeoModelSerializer, HyperlinkedModelSerializer):
     settings = RegionSettingsSerializer(source='regionsettings')
 
     class Meta:
