ace8a6684b6cfa808fea27dcedd641f2db8891ff
Philip Neustrom <philipn@gmail.com>
2014-02-21 18:56:52 +0700

    Little user icon in right hand corner.

--- localwiki/users/templatetags/user_tags.py
+++ localwiki/users/templatetags/user_tags.py
@@ -11,10 +11,10 @@ def user_link(user, size=20, region=None):
         # Get twice as big for high-DPI / retina
         gravatar_url = get_gravatar_url(user.userprofile.gravatar_email, size=(size*2))
         if region:
-            user_path= '%s/%s' % (region.slug, user.get_absolute_url())
+            user_path= '/%s%s' % (region.slug, user.get_absolute_url())
         else:
             region_path = user.get_absolute_url()
-        link = """<a href="%(user_path)s"><img class="gravatar" src="%(gravatar_url)s" width="%(width)s" height="%(height)s" /> %(username)s</a>""" % {
+        link = """<a href="%(user_path)s" class="user_link"><img class="gravatar" src="%(gravatar_url)s" width="%(width)s" height="%(height)s" /> %(username)s</a>""" % {
             'user_path': user_path,
             'gravatar_url': gravatar_url,
             'username': user.username,
