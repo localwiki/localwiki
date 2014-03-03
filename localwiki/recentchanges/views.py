ae3bfbf204b5d9ed8557f49ef7c60a6093c64ac3
Philip Neustrom <philipn@gmail.com>
2014-03-03 15:10:44 +0700

    Grouped-by-slug RC. No longer by-day.

--- localwiki/recentchanges/views.py
+++ localwiki/recentchanges/views.py
@@ -12,7 +12,7 @@ from regions.views import RegionMixin
 from utils import merge_changes
 from recentchanges import get_changes_classes
 
-MAX_DAYS_BACK = 7
+MAX_CHANGES = 500
 IGNORE_TYPES = [
     TYPE_DELETED_CASCADE,
     TYPE_REVERTED_DELETED_CASCADE,
@@ -22,7 +22,7 @@ IGNORE_TYPES = [
 
 class RecentChangesView(RegionMixin, ListView):
     template_name = "recentchanges/recentchanges.html"
-    context_object_name = 'changes_grouped_by_day'
+    context_object_name = 'changes'
 
     def format_change_set(self, change_obj, change_set):
         for obj in change_set:
@@ -34,19 +34,18 @@ class RecentChangesView(RegionMixin, ListView):
 
     def get_queryset(self):
         change_sets = []
-        start_at = self._get_start_date()
         region = self.get_region()
 
         for change_class in get_changes_classes():
             change_obj = change_class(region=region)
-            change_set = change_obj.queryset(start_at)
+            change_set = change_obj.queryset()[:MAX_CHANGES]
             change_sets.append(
                 self.format_change_set(change_obj, change_set))
 
         # Merge the sorted-by-date querysets.
         objs = merge_changes(change_sets)
 
-        return self._changes_grouped_by_day(objs)
+        return self._changes_grouped_by_slug(objs)
 
     def get_context_data(self, *args, **kwargs):
         c = super(RecentChangesView, self).get_context_data(*args, **kwargs)
@@ -60,80 +59,30 @@ class RecentChangesView(RegionMixin, ListView):
         })
         return c
 
-    def _changes_grouped_by_day(self, objs):
+    def _changes_grouped_by_slug(self, objs):
         """
-        Args:
-            objs: A grouped-by-day list of changes.
-
         Returns:
             A list of the form:
             [
-              {'day': datetime representing the day,
-               'changes': a slug-grouped list of changes
-              },
-              ...
+               [ changes, changes, ... ],
+               [ changes, changes, ... ],
+               ...
             ]
 
-            the slug-grouped list of changes is of the form:
-            [ changes, changes, ...]
-
-            where 'changes' are each a list of changes associated
-            with a given slug.  The list-of-changes-lists is ordered
-            by the most recent edit in each 'changes' list.
+            where each group of [ changes, ..] is grouped by slug.
+            The list is ordered by the most recent edit in each
+            [changes, ..] list.
         """
-        def _the_day(o):
-            date = o.version_info.date
-            return (date.year, date.month, date.day)
+        slug_dict = {}
+        # group changes by slug.
+        for change in objs:
+            changes_for_slug = slug_dict.get(change.slug, [])
+            changes_for_slug.append(change)
+            slug_dict[change.slug] = changes_for_slug
 
-        l = []
-        # Group objs by day.
-        for day, changes in groupby(objs, _the_day):
-            slug_dict = {}
-            # For each day, group changes by slug.
-            for change in changes:
-                changes_for_slug = slug_dict.get(change.slug, [])
-                changes_for_slug.append(change)
-                slug_dict[change.slug] = changes_for_slug
+        # Sort the slug_dict by the most recent edit date of each
+        # slug's set of changes.
+        by_most_recent_edit = sorted(slug_dict.values(),
+            key=lambda x: x[0].version_info.date, reverse=True)
 
-            # Sort the slug_dict by the most recent edit date of each
-            # slug's set of changes.
-            by_most_recent_edit = sorted(slug_dict.values(),
-                key=lambda x: x[0].version_info.date, reverse=True)
-
-            # Without loss of generality, grab a datetime object
-            # representing the day.
-            the_day = slug_dict.values()[0][0].version_info.date
-
-            l.append({'day': the_day, 'changes': by_most_recent_edit})
-
-        return l
-
-    def _get_start_date(self):
-        days_back = int(self.request.GET.get('days_back', '2'))
-        if days_back > MAX_DAYS_BACK:
-            raise Http404("Days must be less than %s" % MAX_DAYS_BACK)
-
-        # If days_back is N we will (try and) show N days worth of changes,
-        # not simply the latest N days' changes.  We always want to show as
-        # many changes on the RC page as possible to encourage more wiki
-        # activity.
-        num_days_total = 1
-        region = self.get_region()
-        try:
-            latest_changes = Page.versions.filter(region=region)[0:300]
-            start_at = Page.versions.filter(region=region)[0].version_info.date
-
-            for change in latest_changes:
-                if change.version_info.date.day != start_at.day:
-                    start_at = change.version_info.date
-                    num_days_total += 1
-                if num_days_total == days_back:
-                    break
-        except IndexError:
-            start_at = datetime.datetime.now()
-
-        days_back = datetime.timedelta(days=num_days_total)
-
-        # Set to the beginning of that day.
-        return datetime.datetime(start_at.year, start_at.month, start_at.day,
-            0, 0, 0, 0, start_at.tzinfo)
+        return by_most_recent_edit
