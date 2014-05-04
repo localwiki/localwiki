490eb311017c703fbee3df06386c58f6a9369564
Philip Neustrom <philipn@gmail.com>
2014-05-03 18:58:29 -0700

    Current work on disabling account, not quite done.

--- localwiki/users/urls.py
+++ localwiki/users/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 from registration.views import register
 
-from .views import UserPageView, UserSettingsView
+from .views import UserPageView, UserSettingsView, UserDeactivateView
 
 
 urlpatterns = patterns('',
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
             template_name='registration/registration_closed.html'),
         name='registration_disallowed'),
     url(r'^_settings/?$', UserSettingsView.as_view(), name="user-settings"),
+    url(r'^_deactivate/?$', UserDeactivateView.as_view(), name="user-deactivate"),
     (r'', include('registration.auth_urls')),
     (r'(?P<username>[^/]*)/*(?P<rest>.*)', UserPageView.as_view()),
 )
