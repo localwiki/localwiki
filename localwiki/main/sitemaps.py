4814b7f7aa589cc12d40b8cdf0dfc665fac4a139
Philip Neustrom <philipn@gmail.com>
2014-05-04 20:03:00 -0700

    Radically speed up sitemap generation time.

--- localwiki/main/sitemaps.py
+++ localwiki/main/sitemaps.py
@@ -1,22 +1,54 @@
+from collections import namedtuple
+
 from django.contrib.sitemaps import Sitemap
+from django.db import connection
 from django.core.urlresolvers import reverse
 
 from regions.models import Region
-from pages.models import Page
+from pages.models import Page, page_url
 
+
+def pages_with_last_mod():
+    # We have a lot of pages, and the naive way
+    # to generate a sitemap here takes like 5 minutes.
+    # This is much faster.
+    cursor = connection.cursor()
+    cursor.execute("""
+SELECT pages_page.name, regions_region.slug, p_by_date.history_date FROM (
+    SELECT
+        slug, max(history_date) as history_date, region_id
+    FROM
+        pages_page_hist
+    GROUP BY
+        slug, region_id
+) as p_by_date, pages_page, regions_region
+WHERE
+    p_by_date.slug=pages_page.slug AND
+    p_by_date.region_id=pages_page.region_id AND
+    regions_region.id=p_by_date.region_id
+""")
+    return cursor.fetchall()
+
+MockRegion = namedtuple('MockRegion', ['slug'])
 
 class PageSitemap(Sitemap):
     def items(self):
-        return Page.objects.defer('content').select_related('region')
+        items = []
+        self.lastmod_lookup = {}
+        
+        for pagename, region_slug, lastmod in pages_with_last_mod():
+            self.lastmod_lookup[(pagename, region_slug)] = lastmod
+            items.append((pagename, region_slug))
+        return items
 
     def lastmod(self, obj):
-        if not obj.versions.all().exists():
-            return
+        return self.lastmod_lookup[obj]
 
-        # Sketchy highly optimized query:
-        most_recent = obj.versions.only('history_date')[0]
-        return most_recent.history_date
-
+    def location(self, obj):
+        pagename, region_slug = obj
+        # Sort of a mock here.
+        return page_url(pagename, MockRegion(region_slug))
+        
 
 class RegionSitemap(Sitemap):
     def items(self):
